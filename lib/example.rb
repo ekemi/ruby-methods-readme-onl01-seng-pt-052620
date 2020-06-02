491d835403afdfa041dc10465be9b40d11cbfff5
edmundo papi <edmundopierre@yahoo.com>
2020-06-02 02:57:18 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,17 +1,11 @@
 # Use this space to code along with the readme
-def greeting
-
-  puts "Hello World"
-
-end
-# puts phrase
-# puts phrase
-# puts phrase
-# puts phrase
-# puts phrase
-# puts phrase
-# puts phrase
-# puts phrase
-# puts phrase
-# puts phrase
-greeting
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
